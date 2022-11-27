55fa82a2d1cae7ec3b9342e81e7bf1e412d673f5
Jacob Vielweber <jnvielweber@gmail.com>
2022-11-26 21:28:31 -0600

    Create square component

--- src/components/square.jsx
+++ src/components/square.jsx
@@ -1,2 +1,30 @@
-
 // a div that accepts an onClick funcion and true/false props
+
+import PropTypes from "prop-types";
+
+export default function Square({ room, id, handleClick, marker }) {
+  // TODO: add ternary, if room is true make div white and add onClick function, else make dive black with no onClick function
+
+  return (
+    <div
+      id={id}
+      className={room ? "bg-white" : "bg-black"}
+      onClick={handleClick}
+    >
+      {marker}
+    </div>
+  );
+}
+
+Square.propTypes = {
+  room: PropTypes.bool,
+  id: PropTypes.string.isRequired,
+  handleClick: PropTypes.func,
+  marker: PropTypes.string,
+};
+
+Square.defaultProps = {
+  handleClick: () => {},
+  room: true,
+  marker: "",
+};
