91e37b9991a5aacb80add07f503689a239b13967
Jacob Vielweber <jnvielweber@gmail.com>
2022-12-09 20:36:53 -0600

    Create Login Component

---
+++ src/components/Forms/LoginForm.jsx
@@ -0,0 +1,36 @@
+import Input from "./Input"; // see import.js in forms
+
+const fields = [
+  {
+    id: "email",
+    label: "Email",
+    type: "email",
+  },
+  {
+    id: "password",
+    label: "Password",
+    type: "password",
+  },
+];
+
+export default function Registration() {
+  // link to registration page and about page below form
+  return (
+    <>
+      <form className="w-max mx-auto flex flex-col gap-y-4 border p-8 rounded-md my-4 mt-4">
+        {fields.map((field) => (
+          <Input key={field.id} {...field} />
+        ))}
+
+        <button
+          type="submit"
+          className="px-4 py-2 rounded bg-blue-500 text-white"
+        >
+          Login
+        </button>
+      </form>
+    </>
+  );
+}
+
+Registration.propTypes = {};
