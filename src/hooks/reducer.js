cf8dcccded2de15f3d58f509af59f492a56a708e
Jacob Vielweber <jnvielweber@gmail.com>
2022-12-12 03:44:12 -0600

    Make single hook prepare to test login with reducer

--- src/hooks/reducer.js
+++ src/hooks/reducer.js
@@ -1,8 +1,30 @@
 /*
-* needs to hold
-* map
-* player location
-* and has functions for
-* clicking on square
-* ...
-*/
+ * needs to hold
+ * map
+ * player location
+ * and has functions for
+ * clicking on square
+ * ...
+ */
+
+export default function reducer(state, action) {
+  switch (action.type) {
+    case "user_login": {
+      // TODO: make a test for either this with useGame
+      // make a new user object set to user info attached to the action
+      const newUser = {
+        fName: action.user.firstName,
+        lName: action.user.lastName,
+        email: action.user.email,
+        id: action.user.id,
+      };
+      return {
+        // everthing except the user should be null
+        user: newUser,
+        game: null,
+      };
+    }
+    default:
+      throw new Error("Invalid action");
+  }
+}
---
+++ src/hooks/useGame.js
@@ -0,0 +1,34 @@
+import { useReducer } from "react";
+import reducer from "./reducer";
+
+// lets see if we can split game and login into separate hooks
+
+export default function useGame() {
+  // start with a null user and game
+  const [state, dispatch] = useReducer(reducer, {
+    user: null,
+    game: null,
+  });
+  const { user, game } = state;
+
+  const setUser = (userData) => {
+    dispatch({ type: "user_login", user: userData });
+  };
+  // this works for this project
+  const makeMove = (event) => {
+    dispatch({ type: "made_move", index: event.target.id });
+  };
+
+  const resetGame = () => {
+    dispatch({ type: "reset_game" });
+  };
+
+  // removed declare winner
+  return {
+    user,
+    game,
+    setUser,
+    makeMove,
+    resetGame,
+  };
+}
