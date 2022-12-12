b8ee5c34998f85e9550fc479e9528595d481f41c
Jacob Vielweber <jnvielweber@gmail.com>
2022-12-12 00:02:14 -0600

    Add placeholder getUser function

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -11,4 +11,15 @@ export default {
     });
     return response.json();
   },
+  // use email and password to get user
+  async getUser(email, pass) {
+    const response = await fetch(BASE_URL);
+
+    const usersData = await response.json();
+
+    // TODO: Resolve or reject a promise
+
+    // look for email and password match.
+    return usersData.find((u) => u.email === email && u.password === pass);
+  },
 };
