d47108c4c4eed0d274e89afe44c54c1ded51e671
Jacob Vielweber <jnvielweber@gmail.com>
2022-12-13 16:32:40 -0600

    Implement logout functionality

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,8 +1,20 @@
 import PropTypes from "prop-types";
+import { useNavigate } from "react-router-dom";
+import useGame from "../hooks/useGame";
 
 export default function LogoutBtn() {
   // use either reducer or props to get userName
   // on click function to logout then remove user info from reducer
+  const { clearUser } = useGame();
+  const navigate = useNavigate();
+  // call logout hook and then useNavigate to root
+  function logout() {
+    clearUser();
+    navigate("/");
+    // need to reload or logout button stays on screen
+    window.location.reload(false);
+    // why is this necessary?
+  }
 
   return (
     <>
@@ -10,7 +22,7 @@ export default function LogoutBtn() {
       <button
         type="button"
         className="rounded bg-blue-400 py-2 px-4 font-bold text-white hover:bg-blue-100"
-        onClick={null}
+        onClick={logout}
       >
         Logout
       </button>
