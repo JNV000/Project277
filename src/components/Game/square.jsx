80b5096adff11f601a4b666c19b2ef969bea29a3
Jacob Vielweber <jnvielweber@gmail.com>
2022-12-13 18:51:18 -0600

    Make walls disabled and not clickable

--- src/components/Game/square.jsx
+++ src/components/Game/square.jsx
@@ -1,6 +1,7 @@
 // a div that accepts an onClick funcion and true/false props
 
 import PropTypes from "prop-types";
+import { resolveProjectReferencePath } from "typescript";
 
 export default function Square({ room, id, handleClick, marker }) {
   // TODO: add ternary, if room is true make div white and add onClick function, else make dive black with no onClick function
@@ -13,6 +14,7 @@ export default function Square({ room, id, handleClick, marker }) {
           ? "bg-white h-20 w-20 border-2 col-span-1 row-span1 text-4xl"
           : "bg-black h-20 w-20 col-span-1 row-span1"
       }
+      disabled={!room}
       onClick={handleClick}
     >
       {marker}
